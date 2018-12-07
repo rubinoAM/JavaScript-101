806047492902f9646b7919934ed0b58ca43fe32b
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-07 10:05:38 -0500

    Basketball Exercise & Others

---
+++ dragon.js
@@ -0,0 +1,86 @@
+//Stored Functions
+function rollDie(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min + 1)) + min;}
+
+//Create Dragon
+var dragonImg = new Image(631,463);
+dragonImg.src = "./dragon-assets/dragon.png";
+var dragonDiv = document.createElement("div");
+dragonDiv.id = "dragon";
+
+//Create Dice
+var dieOne = new Image();
+dieOne.src = "./dragon-assets/d1.gif";
+var dieTwo = new Image();
+dieTwo.src = "./dragon-assets/d1.gif";
+var diceDiv = document.createElement("div");
+diceDiv.id = "dice-container";
+
+//Create Game Prompt
+var gamePrompt = document.createElement("div");
+gamePrompt.id = "text-box";
+promptText = document.createElement("p");
+promptText.innerHTML = "Hark! The dragon doth approacheth! Thou must decide thine course of action post haste! Shall ye flee for thine own life, or shall ye attempt a stand against the dreaded beast? If thou chooses to battle the dragon, thou must roll two dice and amass a total roll of 9 or greater. Godspeed ye, brave soul!";
+
+//Create Buttons
+var fleeBtn = document.createElement("button");
+fleeBtn.id = "flee-btn";
+fleeBtn.innerHTML = "FLEE";
+
+var fightBtn = document.createElement("button");
+fightBtn.id = "fight-btn";
+fightBtn.innerHTML = "FIGHT";
+
+var btnDiv = document.createElement("div");
+btnDiv.id = "btn-container";
+
+//Append assets to DIV's
+var gameInput = document.createElement("div");
+gameInput.id = "game-input";
+
+dragonDiv.appendChild(dragonImg);
+diceDiv.appendChild(dieOne);
+diceDiv.appendChild(dieTwo);
+gamePrompt.appendChild(promptText);
+btnDiv.appendChild(fleeBtn);
+btnDiv.appendChild(fightBtn);
+gameInput.appendChild(gamePrompt);
+gameInput.appendChild(btnDiv);
+
+//Append DIV's to <body>
+document.body.appendChild(dragonDiv);
+document.body.appendChild(diceDiv);
+document.body.appendChild(gameInput);
+
+//Check button presses
+document.getElementById("flee-btn").onclick = function(){
+    dragonImg.src = "https://c1.staticflickr.com/3/2463/3633230337_6e4f9c4f0a_b.jpg";
+    promptText.innerHTML = "Alas! Thou hast chosen to flee! Toodle-oo, you cowardly sod!"
+
+    document.getElementById("flee-btn").style.display = "none";
+    document.getElementById("fight-btn").style.display = "none";
+}
+
+document.getElementById("fight-btn").onclick = function(){
+    var rollOne, rollTwo;
+    rollOne = rollDie(1,6);
+    rollTwo = rollDie(1,6);
+    console.log(rollOne + " " + rollTwo);
+    totalRoll = rollOne + rollTwo;
+
+    dieOne.src = "./dragon-assets/d" + rollOne + ".gif";
+    dieTwo.src = "./dragon-assets/d" + rollTwo + ".gif";
+
+    if(totalRoll >= 9){
+        dragonImg.src = "http://discofreestyleplatinumseries.com/wp-content/uploads/2018/02/Disco-Freestyle-Platnium-Series-Preformance-Picture.jpg";
+        promptText.innerHTML = "Somehow you managed to beat the dragon. Good job, bumblechump!";
+    } else {
+        dragonImg.src = "https://c1.staticflickr.com/9/8058/8212846017_d3d620a42f_b.jpg";
+        promptText.innerHTML = "You are dead. Not a big surprise considering you were going up against a dragon!"
+    }
+
+    document.getElementById("flee-btn").style.display = "none";
+    document.getElementById("fight-btn").style.display = "none";
+}
---
+++ js-ex_12-7-18/basketball.js
@@ -0,0 +1,203 @@
+// Define players array
+var players = [
+    {
+      name: 'Paul Millsap',
+      position: 'PF',
+      avgMinutesPlayed: 36,
+      avgPoints: 16.4,
+      avgRebounds: 9.4,
+      starter: true
+    },
+    {
+      name: 'Jeff Teague',
+      position: 'PG',
+      avgMinutesPlayed: 28.6,
+      avgPoints: 15.6,
+      avgRebounds: 1.9,
+      starter: true
+    },
+    {
+      name: 'Al Horford',
+      position: 'C',
+      avgMinutesPlayed: 32,
+      avgPoints: 13.2,
+      avgRebounds: 6.8,
+      starter: true
+    },
+    {
+      name: 'Kent Bazemore',
+      position: 'SF',
+      avgMinutesPlayed: 31.8,
+      avgPoints: 12,
+      avgRebounds: 6.6,
+      starter: true
+    },
+    {
+      name: 'Kyle Korver',
+      position: 'SG',
+      avgMinutesPlayed: 32.4,
+      avgPoints: 11.2,
+      avgRebounds: 4.9,
+      starter: true
+    },
+    {
+      name: 'Dennis Schroder',
+      position: 'PG',
+      avgMinutesPlayed: 18.3,
+      avgPoints: 10.7,
+      avgRebounds: 1.8,
+      starter: false
+    },
+    {
+      name: 'Kris Humphries',
+      position: 'PF',
+      avgMinutesPlayed: 14.7,
+      avgPoints: 9.7,
+      avgRebounds: 5.7,
+      starter: false
+    },
+    {
+      name: 'Mike Scott',
+      position: 'PF',
+      avgMinutesPlayed: 17.6,
+      avgPoints: 7.0,
+      avgRebounds: 3.6,
+      starter: false
+    },
+    {
+      name: 'Thabo Sefolosha',
+      position: 'SF',
+      avgMinutesPlayed: 18.9,
+      avgPoints: 4.8,
+      avgRebounds: 3.9,
+      starter: false
+    },
+    {
+      name: 'Mike Muscala',
+      position: 'PF',
+      avgMinutesPlayed: 7.4,
+      avgPoints: 2.7,
+      avgRebounds: 1.9,
+      starter: false
+    },
+    {
+      name: 'Tim Hardaway Jr.',
+      position: 'SG',
+      avgMinutesPlayed: 9.7,
+      avgPoints: 2.2,
+      avgRebounds: 1.0,
+      starter: false
+    },
+    {
+      name: 'Lamar Patterson',
+      position: 'SG',
+      avgMinutesPlayed: 5.0,
+      avgPoints: 1.5,
+      avgRebounds: 1.3,
+      starter: false
+    },
+    {
+      name: 'Kirk Hinrich',
+      position: 'SG',
+      avgMinutesPlayed: 4.5,
+      avgPoints: 0.8,
+      avgRebounds: 0.7,
+      starter: false
+    }
+  ];
+
+// Print the average playing time of the players
+function calcAvgTimePlayers(){
+    var avgOfAvgMinutes = 0;
+    for(i=0; i<players.length; i++){
+    avgOfAvgMinutes += players[i].avgMinutesPlayed;
+    }
+    console.log((avgOfAvgMinutes/players.length).toFixed(1));
+}
+
+// Print the average playing time of the starters
+function calcAvgTimeStarters(){
+    var avgOfAvgMinutes = 0;
+    var numStarters = 0;
+    for(i=0; i<players.length; i++){
+        if (players[i]['starter']){
+            avgOfAvgMinutes += players[i].avgMinutesPlayed;
+            numStarters += 1;
+        }
+    }
+    console.log((avgOfAvgMinutes/numStarters).toFixed(1));
+}
+
+// Print the average playing time of the bench players
+function calcAvgTimeBenchPlayers(){
+    var avgOfAvgMinutes = 0;
+    var numBench = 0;
+    for(i=0; i<players.length; i++){
+        if (players[i]['starter'] == false){
+            avgOfAvgMinutes += players[i].avgMinutesPlayed;
+            numBench += 1;
+        }
+    }
+    console.log((avgOfAvgMinutes/numBench).toFixed(1));
+}
+
+// Create an array of the names of each player
+function arrayPlayerNames(){
+    var playerArray = [];
+    for(i=0; i<players.length; i++){
+        playerArray.push(players[i]['name']);
+    }
+    console.log(playerArray);
+}
+
+// Create an array of the names of the players who average more than 10 points per game
+function moreThanTenPointsPerGame(){
+    var playerArray = [];
+    for(i=0; i<players.length; i++){
+        if(players[i]['avgPoints'] > 10){
+            playerArray.push(players[i]['name']);
+        }
+    }
+    console.log(playerArray);
+}
+
+// Create an array of the names of the players who average more than 5 rebounds per game
+function moreThanFiveReboundsPerGame(){
+    var playerArray = [];
+    for(i=0; i<players.length; i++){
+        if(players[i]['avgRebounds'] > 5){
+            playerArray.push(players[i]['name']);
+        }
+    }
+    console.log(playerArray);
+}
+
+// Determine which player has the most points played per minute
+function mostPointsPerMinute(){
+    var playerMostPointsPerMinute = "";
+    for(i=0; i<players.length-1; i++){
+        if (players[i+1]["avgPoints"]/players[i+1]["avgMinutesPlayed"] > players[i]["avgPoints"]/players[i]["avgMinutesPlayed"]){
+            playerMostPointsPerMinute = players[i+1]["name"];
+        }
+    }
+    console.log(playerMostPointsPerMinute);
+}
+
+// Determine the average points score for the team as a whole
+function avgTeamScore(){
+    var totalTeamScore = 0;
+    for(i=0; i<players.length; i++){
+        totalTeamScore += players[i]["avgPoints"];
+    }
+    console.log((totalTeamScore/players.length).toFixed(1));
+}
+
+// Call all functions
+calcAvgTimePlayers();
+calcAvgTimeStarters();
+calcAvgTimeBenchPlayers();
+arrayPlayerNames();
+moreThanTenPointsPerGame();
+moreThanFiveReboundsPerGame();
+mostPointsPerMinute();
+avgTeamScore();
