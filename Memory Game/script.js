0cec286da8a5a63d0b229b398dc849d83bb2d737
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-17 13:52:37 -0500

    Images

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -1,1 +1,14 @@
-console.log("Yeah.")
+$(document).ready(()=>{
+    $('button').click(function(){
+        let gridSize = $(this).attr('diff');
+        console.log(gridSize);
+        let cards = [];
+        for(let i=0;i<gridSize/2;i++){
+            let monsterNumber = 1;
+            if(i<10){
+                monsterNumber = "0"+i;
+            }
+            cards.push(`<img src-="/img/monsters-${monsterNumber}.png" />`);
+        }    
+    })
+})
