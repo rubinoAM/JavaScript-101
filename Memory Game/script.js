0e37d8637ebf5aa9c81aec82f315188afa9a1abd
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-18 08:59:09 -0500

    Win/Lose Alerts

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -1,5 +1,7 @@
 $(document).ready(()=>{
     $('button').click(function(){
+        let numGuesses = 0;
+        let numMatches = 0;
         let gridSize = $(this).attr('diff');
         let cards = [];
         for(let i=1;i<=gridSize/2;i++){ //Our monsters start at 1 in their filenames
@@ -27,6 +29,7 @@ $(document).ready(()=>{
         $('.playcard-holder').click(function(){
             $(this).addClass('flip');
             let cardsUp = $('.flip');
+            numGuesses += 1;
             if (cardsUp.length === 2){
                 const card1 = cardsUp[0];
                 const card2 = cardsUp[1];
@@ -34,12 +37,16 @@ $(document).ready(()=>{
                 if (card1.innerHTML == card2.innerHTML){
                     cardsUp.removeClass('flip');
                     cardsUp.addClass('match');
+                    numMatches += 1;
                 } else {
                     setTimeout(()=>{
                         cardsUp.removeClass('flip'); //The user needs to see the card before we flip it back. Use setTimeout()
                     },1000);
                 }
             }
+            checkWin(numGuesses,numMatches,gridSize);
+            console.log(numGuesses);
+            console.log(numMatches);
         });
     })
 })
@@ -53,4 +60,31 @@ function shuffleDeck(d){
         d[rand2] = safeCard;
     }
     return d;
+}
+
+function checkWin(guesses,matches,size){
+    if (size == 4){
+        if (guesses > 8){
+            alert("You Lose!");
+        }
+        else if (matches == 2){
+            alert("You Win!");
+        }
+    }
+    else if (size == 12){
+        if (guesses > 24){
+            alert("You Lose!");
+        }
+        else if (matches == 6){
+            alert("You Win!");
+        }
+    }
+    else if (size == 28){
+        if (guesses > 56){
+            alert("You Lose!");
+        }
+        else if (matches == 14){
+            alert("You Win!");
+        }
+    }
 }
